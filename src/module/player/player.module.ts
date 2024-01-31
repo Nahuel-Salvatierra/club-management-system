942a58c102a2961071ac6f9e220c6cab54562086
Nahuel <salvatierra.nahuel@gmai.com>
2024-01-31 15:02:51 -0300

    Add common module

--- src/module/player/player.module.ts
+++ src/module/player/player.module.ts
@@ -4,12 +4,14 @@ import { PlayerService } from './application/service/player.service';
 import { PlayerEntity } from './infrastructure/persistance/entities/player.entity';
 import { PlayerController } from './controller/player.controller';
 import { PlayerMysqlRepository } from './infrastructure/persistance/player.mysql.repository';
+import { CommonModule } from 'src/common/common.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([PlayerEntity])],
   controllers: [PlayerController],
   providers: [
     PlayerService,
+    CommonModule,
     {
       provide: 'PLAYER_REPOSITORY',
       useClass: PlayerMysqlRepository,
