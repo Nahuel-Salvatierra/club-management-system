3534130bc27dffe48aba7551211142de8dce7816
Nahuel <salvatierra.nahuel@gmai.com>
2024-02-01 12:44:49 -0300

    Add fixtures player

--- src/module/player/controller/__test__/player.controller.spec.ts
+++ src/module/player/controller/__test__/player.controller.spec.ts
@@ -3,11 +3,18 @@ import { Test } from '@nestjs/testing';
 import * as request from 'supertest';
 
 import { AppModule } from './../../../../app.module';
-import { player1 } from './player.fixture';
+import {
+  playerCreateDto,
+  playerFixture1,
+  playerUpdateDto,
+} from './player.fixture';
 import { Player } from '../../domain/player.domain';
+import { LoaderService } from './../../../../common/application/loader/loader.service';
+import { fixtureTree } from './../../../../common/fixtures/fixtureTree';
 
 describe('Player Controller', () => {
   let app: INestApplication;
+  let loaderService: LoaderService;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -15,23 +22,72 @@ describe('Player Controller', () => {
     }).compile();
     app = moduleRef.createNestApplication();
     await app.init();
+    loaderService = app.get<LoaderService>(LoaderService);
+  });
+
+  beforeEach(async () => {
+    await loaderService.loadFixtures(fixtureTree);
+  });
+
+  afterEach(async () => {
+    await loaderService.dropFixtures();
   });
 
   describe('POST /player', () => {
-    it('Should be create a new player', async () => {
+    it('Should create a new player', async () => {
       const { body }: { body: Player } = await request(app.getHttpServer())
         .post('/player')
-        .send(player1);
-      expect(body).toHaveProperty('id', 1);
+        .send(playerCreateDto);
+      expect(body).toHaveProperty('id', 2);
       expect(body.dateOfBirth).toBeDefined();
-      expect(body.firstName).toBe(player1.firstName);
-      expect(body.lastName).toBe(player1.lastName);
-      expect(body.name).toBe(player1.name);
-      expect(body.nationality).toBe(player1.nationality);
-      expect(body.position).toBe(player1.position);
-      expect(body.shirtNumber).toBe(player1.shirtNumber);
+      expect(body.firstName).toBe(playerCreateDto.firstName);
+      expect(body.lastName).toBe(playerCreateDto.lastName);
+      expect(body.name).toBe(playerCreateDto.name);
+      expect(body.nationality).toBe(playerCreateDto.nationality);
+      expect(body.position).toBe(playerCreateDto.position);
+      expect(body.shirtNumber).toBe(playerCreateDto.shirtNumber);
       expect(body).toHaveProperty('createdAt');
       expect(body).toHaveProperty('updatedAt');
+    });
+  });
+
+  describe('GET /player', () => {
+    it('Should get array of player', async () => {
+      const { body } = await request(app.getHttpServer()).get('/player');
+      expect(body).toHaveLength(1);
+      expect(body[0]).toHaveProperty('name', playerFixture1.name);
+      expect(body[0]).toHaveProperty('firstName', playerFixture1.firstName);
+      expect(body[0]).toHaveProperty('lastName', playerFixture1.lastName);
+      expect(body[0]).toHaveProperty('nationality', playerFixture1.nationality);
+      expect(body[0]).toHaveProperty('position', playerFixture1.position);
+      expect(body[0]).toHaveProperty('shirtNumber', playerFixture1.shirtNumber);
+      expect(body[1]).toBeUndefined();
+    });
+
+    it('Should get a player by ID', async () => {
+      const { body } = await request(app.getHttpServer()).get('/player/1');
+      expect(body).toHaveProperty('name', playerFixture1.name);
+      expect(body).toHaveProperty('firstName', playerFixture1.firstName);
+      expect(body).toHaveProperty('lastName', playerFixture1.lastName);
+      expect(body).toHaveProperty('nationality', playerFixture1.nationality);
+      expect(body).toHaveProperty('position', playerFixture1.position);
+      expect(body).toHaveProperty('shirtNumber', playerFixture1.shirtNumber);
+    });
+
+    it('Should throw 404 if player not found', async () => {
+      await request(app.getHttpServer()).get('/player/999').expect(404);
+    });
+  });
+
+  describe('/PUT /player', () => {
+    it('Should update a player', async () => {
+      const { body } = await request(app.getHttpServer())
+        .put('/player/1')
+        .send(playerUpdateDto);
+      expect(body).toHaveProperty('id', 1);
+      expect(body).toHaveProperty('name', playerUpdateDto.name);
+      expect(body).toHaveProperty('lastName', playerUpdateDto.lastName);
+      expect(body).toHaveProperty('shirtNumber', playerCreateDto.shirtNumber);
     });
   });
 });
